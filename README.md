bc1af3c1197e639d66c43c73b9a37e39aca29b41
pgh97 <pgh97@com2us.com>
2022-02-16 19:11:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# my-slim-api-test
+
+---
+### 참고 사이트 : https://github.com/maurobonfietti/rest-api-slim-php
---
+++ src/Controller/BaseController.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Slim\Container;
+use Slim\Http\Response;
+
+abstract class BaseController
+{
+    protected Container $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param array|object|null $message
+     */
+    protected function jsonResponse(
+        Response $response,
+        string $status,
+        $message,
+        int $code
+    ): Response {
+        $result = [
+            'code' => $code,
+            'status' => $status,
+            'message' => $message,
+        ];
+
+        return $response->withJson($result, $code, JSON_PRETTY_PRINT);
+    }
+
+    protected static function isRedisEnabled(): bool
+    {
+        return filter_var($_SERVER['REDIS_ENABLED'], FILTER_VALIDATE_BOOLEAN);
+    }
+}
